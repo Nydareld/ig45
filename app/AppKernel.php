001a310538dc3a68341fe87bc6e75b8b488080bd
Theo Guerin <theo.guerin.pro@gmail.com>
2017-03-13 13:28:25 +0100

    requrire bundle utilisateur

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -20,6 +20,7 @@ class AppKernel extends Kernel
             new BackofficeBaseBundle\BackofficeBaseBundle(),
             new UserBundle\UserBundle(),
             new AgendaBundle\AgendaBundle(),
+            new FOS\UserBundle\FOSUserBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
