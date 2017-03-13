4cf7c018c678e82c4030479aef9962ceffd0ccdc
Theo Guerin <theo.guerin.pro@gmail.com>
2017-03-13 13:32:45 +0100

    ajout des champs nom et prenom a l'utilisateur

--- src/UserBundle/Entity/User.php
+++ src/UserBundle/Entity/User.php
@@ -17,9 +17,70 @@ class User extends BaseUser
      * @ORM\GeneratedValue(strategy="AUTO")
      */
     protected $id;
+    protected $nom;
+    protected $prenom;
 
     public function __construct()
     {
         parent::__construct();
     }
+
+    /**
+     * Get the value of Id
+     *
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Get the value of Nom
+     *
+     * @return mixed
+     */
+    public function getNom()
+    {
+        return $this->nom;
+    }
+
+    /**
+     * Set the value of Nom
+     *
+     * @param mixed nom
+     *
+     * @return self
+     */
+    public function setNom($nom)
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of Prenom
+     *
+     * @return mixed
+     */
+    public function getPrenom()
+    {
+        return $this->prenom;
+    }
+
+    /**
+     * Set the value of Prenom
+     *
+     * @param mixed prenom
+     *
+     * @return self
+     */
+    public function setPrenom($prenom)
+    {
+        $this->prenom = $prenom;
+
+        return $this;
+    }
+ 
 }
