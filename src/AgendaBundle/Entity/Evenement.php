ab9a81b7cbf5cb964fdebc33af598acd5a482304
Mathieu Desroches <mathieu.desroches@etu.univ-orleans.fr>
2017-03-14 13:15:29 +0100

    Ajout evenement

---
+++ src/AgendaBundle/Entity/Evenement.php
@@ -0,0 +1,313 @@
+<?php
+
+namespace AgendaBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Evenement
+ *
+ * @ORM\Table(name="evenement")
+ * @ORM\Entity(repositoryClass="AgendaBundle\Repository\EvenementRepository")
+ */
+class Evenement
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="nom", type="string", length=255)
+     */
+    private $nom;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="description", type="text")
+     */
+    private $description;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="date_evt", type="datetime")
+     */
+    private $dateEvt;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="heure_debut", type="datetime")
+     */
+    private $heureDebut;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="heure_fin", type="datetime")
+     */
+    private $heureFin;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="nb_participants", type="integer")
+     */
+    private $nbParticipants;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="nb_observateurs", type="integer")
+     */
+    private $nbObservateurs;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="niveau_intervention", type="string", length=255)
+     */
+    private $niveauIntervention;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="type_intervention", type="string", length=255)
+     */
+    private $typeIntervention;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set nom
+     *
+     * @param string $nom
+     *
+     * @return Evenement
+     */
+    public function setNom($nom)
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    /**
+     * Get nom
+     *
+     * @return string
+     */
+    public function getNom()
+    {
+        return $this->nom;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     *
+     * @return Evenement
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * Set dateEvt
+     *
+     * @param \DateTime $dateEvt
+     *
+     * @return Evenement
+     */
+    public function setDateEvt($dateEvt)
+    {
+        $this->dateEvt = $dateEvt;
+
+        return $this;
+    }
+
+    /**
+     * Get dateEvt
+     *
+     * @return \DateTime
+     */
+    public function getDateEvt()
+    {
+        return $this->dateEvt;
+    }
+
+    /**
+     * Set heureDebut
+     *
+     * @param \DateTime $heureDebut
+     *
+     * @return Evenement
+     */
+    public function setHeureDebut($heureDebut)
+    {
+        $this->heureDebut = $heureDebut;
+
+        return $this;
+    }
+
+    /**
+     * Get heureDebut
+     *
+     * @return \DateTime
+     */
+    public function getHeureDebut()
+    {
+        return $this->heureDebut;
+    }
+
+    /**
+     * Set heureFin
+     *
+     * @param \DateTime $heureFin
+     *
+     * @return Evenement
+     */
+    public function setHeureFin($heureFin)
+    {
+        $this->heureFin = $heureFin;
+
+        return $this;
+    }
+
+    /**
+     * Get heureFin
+     *
+     * @return \DateTime
+     */
+    public function getHeureFin()
+    {
+        return $this->heureFin;
+    }
+
+    /**
+     * Set nbParticipants
+     *
+     * @param integer $nbParticipants
+     *
+     * @return Evenement
+     */
+    public function setNbParticipants($nbParticipants)
+    {
+        $this->nbParticipants = $nbParticipants;
+
+        return $this;
+    }
+
+    /**
+     * Get nbParticipants
+     *
+     * @return int
+     */
+    public function getNbParticipants()
+    {
+        return $this->nbParticipants;
+    }
+
+    /**
+     * Set nbObservateurs
+     *
+     * @param integer $nbObservateurs
+     *
+     * @return Evenement
+     */
+    public function setNbObservateurs($nbObservateurs)
+    {
+        $this->nbObservateurs = $nbObservateurs;
+
+        return $this;
+    }
+
+    /**
+     * Get nbObservateurs
+     *
+     * @return int
+     */
+    public function getNbObservateurs()
+    {
+        return $this->nbObservateurs;
+    }
+
+    /**
+     * Set niveauIntervention
+     *
+     * @param string $niveauIntervention
+     *
+     * @return Evenement
+     */
+    public function setNiveauIntervention($niveauIntervention)
+    {
+        $this->niveauIntervention = $niveauIntervention;
+
+        return $this;
+    }
+
+    /**
+     * Get niveauIntervention
+     *
+     * @return string
+     */
+    public function getNiveauIntervention()
+    {
+        return $this->niveauIntervention;
+    }
+
+    /**
+     * Set typeIntervention
+     *
+     * @param string $typeIntervention
+     *
+     * @return Evenement
+     */
+    public function setTypeIntervention($typeIntervention)
+    {
+        $this->typeIntervention = $typeIntervention;
+
+        return $this;
+    }
+
+    /**
+     * Get typeIntervention
+     *
+     * @return string
+     */
+    public function getTypeIntervention()
+    {
+        return $this->typeIntervention;
+    }
+}
