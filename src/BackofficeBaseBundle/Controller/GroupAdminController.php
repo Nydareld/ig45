dac1c1699e834d102b178700a4ca6df455cff5ee
Theo Guerin <theo.guerin.pro@gmail.com>
2017-03-17 11:26:56 +0100

    controller de liste + creation de goupes

--- src/BackofficeBaseBundle/Controller/GroupAdminController.php
+++ src/BackofficeBaseBundle/Controller/GroupAdminController.php
@@ -3,9 +3,9 @@
 namespace BackofficeBaseBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use UserBundle\Form\UserType;
+use UserBundle\Form\GroupType;
 use Symfony\Component\HttpFoundation\Request;
-use UserBundle\Entity\User;
+use UserBundle\Entity\Group;
 
 class GroupAdminController extends Controller
 {
@@ -16,10 +16,26 @@ class GroupAdminController extends Controller
      */
     public function groupListAction(Request $request)
     {
+        $groupManager = $this->get('fos_user.group_manager');
+        $group = $groupManager->createGroup('');
+
+        $form = $this->createForm( GroupType::class , $group);
+        $form->setData($group);
+        $form->handleRequest($request);
+        if ($form->isSubmitted()) {
+            if ($form->isValid()) {
+                $groupManager->updateGroup($group);
+                $request->getSession()
+                ->getFlashBag()
+                ->add('success', "le groupe a bien été créé");
+            }
+        }
+
         $groups = $this->get('fos_user.group_manager')->findGroups();
 
         return $this->render('BackofficeBaseBundle:Groups:groupList.html.twig',array(
-            'groups' => $groups
+            'groups' => $groups,
+            'form' => $form->createView()
         ));
 
     }
