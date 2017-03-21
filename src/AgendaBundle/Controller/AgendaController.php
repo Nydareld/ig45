68e3619d8fd49ca33cc6ae5c22cc663344fe6175
Florentin Fortea <florentinfortea4@gmail.com>
2017-03-21 10:12:45 +0100

    Controller + tableau

--- src/AgendaBundle/Controller/AgendaController.php
+++ src/AgendaBundle/Controller/AgendaController.php
@@ -8,14 +8,15 @@ class AgendaController extends Controller
 {
     public function indexAction()
     {
-        // récupération de tout les évènements
-        $evenements = $this->getDoctrine()
-                            ->getRepository('AgendaBundle:Evenement')
-                            ->findAll();
+        // // récupération de tout les évènements
+        // $evenements = $this->getDoctrine()
+        //                     ->getRepository('AgendaBundle:Evenement')
+        //                     ->findAll();
 
         // on envoie tout les évènements au twig
-        return $this->render('AgendaBundle:Agenda:agenda.html.twig',
-                        array('evenements' => $evenements));
+        // return $this->render('AgendaBundle:Agenda:agenda.html.twig',
+        //                 array('evenements' => $evenements));
+        return $this->render('AgendaBundle:Agenda:agenda.html.twig');
     }
 
     public function agendaPersoAction()
