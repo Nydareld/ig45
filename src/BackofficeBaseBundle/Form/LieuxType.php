1cda272195d1917be1def7fc9a53e4e6a755cbfc
Theo Guerin <theo.guerin.pro@gmail.com>
2017-03-21 10:01:26 +0100

    correction du form d'ajout d'établissement ( widgets et translation )

--- src/BackofficeBaseBundle/Form/LieuxType.php
+++ src/BackofficeBaseBundle/Form/LieuxType.php
@@ -5,6 +5,9 @@ namespace BackofficeBaseBundle\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 
 class LieuxType extends AbstractType
 {
@@ -13,7 +16,39 @@ class LieuxType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('nom')->add('adresse')->add('ville')->add('codePostal')->add('correspondants')->add('adjoints')->add('type');
+        $builder
+            ->add('nom',TextType::class,array(
+                'label'=>'lieux.fieldslabel.nom',
+                'translation_domain' => 'AgendaBundle'
+            ))
+            ->add('adresse',TextType::class,array(
+                'label'=>'lieux.fieldslabel.adresse',
+                'translation_domain' => 'AgendaBundle'
+            ))
+            ->add('ville',TextType::class,array(
+                'label'=>'lieux.fieldslabel.ville',
+                'translation_domain' => 'AgendaBundle'
+            ))
+            ->add('codePostal',NumberType::class,array(
+                'label'=>'lieux.fieldslabel.codePostal',
+                'translation_domain' => 'AgendaBundle'
+            ))
+            ->add('correspondants',EntityType::class,array(
+                'class' => 'UserBundle:User',
+                'label'=>'lieux.fieldslabel.correspondants',
+                'translation_domain' => 'AgendaBundle'
+            ))
+            ->add('adjoints', EntityType::class, array(
+                'class' => 'UserBundle:User',
+                'multiple' => true,
+                'label'=>'lieux.fieldslabel.adjoints',
+                'translation_domain' => 'AgendaBundle'
+            ))
+            ->add('type', EntityType::class,array(
+                'class' => 'AgendaBundle:Type_lieu',
+                'label'=>'lieux.fieldslabel.type',
+                'translation_domain' => 'AgendaBundle'
+            ));
     }
 
     /**
