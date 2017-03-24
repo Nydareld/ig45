e0d058ec203abec38908d66c32b8564f87114463
Theo Guerin <theo.guerin.pro@gmail.com>
2017-03-24 13:25:25 +0100

    hotfix group collection user

--- src/UserBundle/Entity/User.php
+++ src/UserBundle/Entity/User.php
@@ -4,6 +4,7 @@ namespace UserBundle\Entity;
 
 use FOS\UserBundle\Model\User as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
+use Doctrine\Common\Collections\ArrayCollection;
 
 /**
  * @ORM\Entity
@@ -77,6 +78,7 @@ class User extends BaseUser
     public function __construct()
     {
         parent::__construct();
+        $this->groups = new ArrayCollection();
     }
 
     /**
