2b24cea1ffe57e830276544710c91f0c92114842
Damien <damien.m.45@gmail.com>
2017-03-27 09:53:45 +0200

    Fixtures Prod + champ presence Etab. + correction
    Début des fixtures de prod (à terminer et modifier celles de dev)
    Ajout d'un champ "presence" dans Etablissement (type = string(3))
    Débug fixtures

--- src/AgendaBundle/Form/EvenementType.php
+++ src/AgendaBundle/Form/EvenementType.php
@@ -65,9 +65,7 @@ class EvenementType extends AbstractType
                     ),
                   )
                 )
-                ->add('observateurs')
-                ->add('niveauIntervention');
-
+                ->add('observateurs');
     }
 
     /**
