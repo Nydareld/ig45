6e492fc6a1a4d56649e19014bc956b1700225f60
Mathieu Desroches <mathieu.desroches@etu.univ-orleans.fr>
2017-03-28 11:33:04 +0200

    Travail sur l'affichage de la vue d'ensemble

--- src/BackofficeBaseBundle/Controller/DefaultController.php
+++ src/BackofficeBaseBundle/Controller/DefaultController.php
@@ -8,6 +8,26 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('BackofficeBaseBundle:Default:index.html.twig');
+        $em = $this->getDoctrine()->getManager();
+
+        $query = $em->createQuery('SELECT count(e.id) as nbE FROM AgendaBundle:Etablissement e');
+        $etablissement = $query->getResult();
+
+        $query = $em->createQuery('SELECT count(ev.id) as nbEv FROM AgendaBundle:Evenement ev');
+        $evenement = $query->getResult();
+
+        $query = $em->createQuery('SELECT count(n.id) as nbN FROM AgendaBundle:Niveau n');
+        $niveau = $query->getResult();
+
+        $query = $em->createQuery('SELECT count(t.id) as nbT FROM AgendaBundle:TypeEvenement t');
+        $typeEvenement = $query->getResult();
+
+
+        return $this->render('BackofficeBaseBundle:Default:index.html.twig',array(
+          'etablissement' => $etablissement[0],
+          'evenement' => $evenement[0],
+          'niveau' => $niveau[0],
+          'typeEvenement' => $typeEvenement[0],
+        ));
     }
 }
